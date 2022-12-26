69e3b50f32a5293d98b1b17876ce38cf85d0ece7
SagarUtwani <sutwani022@gmail.com>
2022-12-26 11:44:08 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+//variables
+
+// console.log('This is external log');
+// console.warn('This is external warning');
+// console.error('This is extarnal error');
+// console.clear();
+
+// num1 = 10;
+// num2 = 5;
+
+// document.write("This is addition ", num1+num2, "<br>", "This is substraction ", num1-num2, "<br>", "This is multiplication ", num1*num2, "<br>", "This is division ", num1/num2, "<br>");
+
+
+// variables datatypes and typeof()
+
+// let a = 10;
+// let b = 'Sagar';
+// let c = true;
+// let d;
+
+// document.write('Datatype of variable a is '+typeof(a)+ "<br>");
+// document.write('Datatype of variable b is '+typeof(b)+ "<br>");
+// document.write('Datatype of variable c is '+typeof(c)+ "<br>");
+// document.write('Datatype of variable d is '+typeof(d)+ "<br>");
+
+//Nonpirimitive data types
+//Array
+// let user = ['Sagar', 'Dilip kumar', 27, 'sutwani022@gmail.com', true, undefined];
+// console.log(user);
+// document.write(user[1]);
+// document.write(user);
+
+//Prompt
+// let a = prompt('Enter first number');
+// document.write(a);
+
+// let cars = ['BMW', 'Buggati', 'Ferrari', 'Corola', 'Honda']
+// let a = prompt('Enter any number from 0 to 4');
+// document.write(cars[a]);
+
+let cars = ['', '', '', '', ''];
+let a = prompt('enter 5 values');
+cars[0] = a;
+let b = prompt('second');
+cars[1] = b
+let c = prompt('third');
+cars[2] = c;
+let d = prompt('forth');
+cars[3] = d;
+let e = prompt('fifth');
+cars[4] = e;
+let f = prompt('put index number Which you wanna see');
+document.write(cars[f]);
+
+
+
